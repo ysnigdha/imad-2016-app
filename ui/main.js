6b8e4b7f50d92a67e5773f368505536e431b67bd
ysnigdha <yadasnigdha@gmail.com>
2016-10-24 15:13:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function()

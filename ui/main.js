e16b5847d97317d71226aadc292f049d50228768
ysnigdha <yadasnigdha@gmail.com>
2016-10-24 15:13:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,13 @@ var element = document.getElementById('main-text');
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft=marginLeft + 'px';
+}
 img.onclick=function()
 {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };

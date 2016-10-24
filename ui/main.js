764c92186283d77eb25655f2c9d7a7261fe6c31b
ysnigdha <yadasnigdha@gmail.com>
2016-10-24 14:52:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 
 //change the text of main-text div
 var element = document.getElementById('main-text');
+
+
+//move the image
+var img = document.getElementById('img');
+img.onClick=function()
+{
+    img.style.marginLeft = "100px";
+};

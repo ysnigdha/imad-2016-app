652114a63661b3129284b7e38c50f29a5ec9ef65
ysnigdha <yadasnigdha@gmail.com>
2016-10-26 11:41:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ button.onclick = function ()
     request.open('GET','http://ysnigdha.imad.hasura-app.io/counter',true);
     request.send(null);
    };
+   //servlet name
+   var nameInput=document.getElementById('name');
+   var name=name.Input.value;
+   var submit=document.getElementById('submit_btn');
+   submit.onclick = function() {
+       //make a request to the server and send the name 
+       //cpture  a list of name and render it as a list
+       var name=["name1","name2","name3"];
+       var list='';
+       for (var i=0;i<names.length;i++) {
+           list+='<li>' +names[i] +'<li>';
+       }
+       var ul=document.getElementById('namelist');
+       ul.innerHTML =list;
+   };
+   

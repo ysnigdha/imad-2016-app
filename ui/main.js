7cd203face8fe55fa1981e0f0dbad9f89244bb0d
ysnigdha <yadasnigdha@gmail.com>
2016-10-26 12:36:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,31 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick = function() {
-    //make a request to the server and send the name
+        //creating a request object
+    var request=new XMLHttpRequest();
+    //capture the response and store  it in a variable
+    request.onreadystatechangec=function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200) {
+                
+                  //capture a list of names and render it as a list
+    var names=request.responseText;
+    names=JSON.parse(names);
+    var list='';
+    for(var i=0;i<names.length;i++) {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+            }
+        }
+        //not yet done
+    };
+//make the request
+request.open('GET','http://ysnigdha.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
+};
     //capture a list of names and render it as a list
     var names=['name1','name2','name3'];
     var list='';
@@ -34,7 +58,3 @@ submit.onclick = function() {
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
 };
-    
-        
-    }
-}

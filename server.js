1d43f8785af2c5c8389bdadb4f3b59186693f160
ysnigdha <yadasnigdha@gmail.com>
2016-10-27 22:16:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'ysnigdha',
     database:'ysnigdha',
-    host:'db.imad.hasura_app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };

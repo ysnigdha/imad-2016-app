eabc5b9710e7d851e772eba1b142e609540a5f7a
ysnigdha <yadasnigdha@gmail.com>
2016-11-17 19:48:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
 var config = {
     user: 'ysnigdha',
     database:'ysnigdha',
@@ -50,6 +52,21 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    //how to create a hash?
+    var hashed =crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+    
+}
+
+app.get('/hash/:input',function(req, res){
+
+var hashedString = hash(req.params.input,'this-is-a-random-string');
+res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
 //select request 

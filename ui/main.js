c8174c7118acb1111ff61d45f1ce7f5a7b684f22
ysnigdha <yadasnigdha@gmail.com>
2016-11-18 15:02:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,3 @@
-//counter code
-var button=document.getElementById('counter');
-button.onclick = function() {
-    //creating a request object
-    var request=new XMLHttpRequest();
-    //capture the response and store  it in a variable
-    request.onreadystatechangec=function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //take some action
-            if(request.status === 200) {
-                var counter=request.responseText;
-                var span= document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //not yet done
-    };
-//make the request
-request.open('GET','http://ysnigdha.imad.hasura-app.io/counter',true);
-request.send(null);
-};
 //submit name
 var submit=document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -31,21 +10,22 @@ submit.onclick = function() {
             if(request.status === 200) {
                 
                   //capture a list of names and render it as a list
-    var names=request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++) {
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-            }
+   console.log('user logged in');
+   alert('logged in successfully');
+            }else if (request.status === 403) {
+                alert('username/password is invalid');
+            }else if (request.status === 500) {
+                alert('something went wrong wit the server');
+        }
         }
         //not yet done
     };
 //make the request
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://ysnigdha.imad.hasura-app.io/submit-name?name=' +name, true);
-request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://ysnigdha.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-type','application/json');
+request.send(JSON.stringify({username :username , password:password}));
 };

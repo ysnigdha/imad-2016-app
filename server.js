c5acc3815793ed8b47ac9b735ad63d73b5cd236f
ysnigdha <yadasnigdha@gmail.com>
2016-11-18 17:27:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,9 +85,7 @@ app.get('/hash/:input', function(req, res) {
 });
 
 app.post('/create-user', function (req, res) {
-   // username, password
-   // {"username": "tanmai", "password": "password"}
-   // JSON
+  
    var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
